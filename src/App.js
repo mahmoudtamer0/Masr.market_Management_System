53d23b34a3cc44866305bb5c0907e07d01d6c9d5
mahmoud <mamoidtamer300@gmail.com>
2023-08-03 05:20:49 +0300

    fixing preloader

--- src/App.js
+++ src/App.js
@@ -12,18 +12,41 @@ import './components/preloader.css'
 
 
 function App() {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true)
+
+    setTimeout(() => {
+      setLoading(false)
+    }, 1500);
+  }, [])
   return (
     <div className="App">
-      <Router basename='/Masr.market_Management_System'>
-        <Navbar />
-        <Routes>
-          <Route path='/' element={<Products />} />
-          <Route path='/:productId' element={<Productdet />} />
-          <Route path='/edit/:producteditId' element={<Editprod />} />
-          <Route path='/add_product' element={<Addproduct />} />
-        </Routes>
-        <div><Footer /></div>
-      </Router>
+      {loading ?
+        <div className="preloader">
+          <HashLoader
+            color='#ed8a8a'
+            loading={loading}
+            size={150}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          /></div>
+        : <div>
+          <Router basename='/Masr.market_Management_System'>
+            <Navbar />
+            <Routes>
+              <Route path='/' element={<Products />} />
+              <Route path='/:productId' element={<Productdet />} />
+              <Route path='/edit/:producteditId' element={<Editprod />} />
+              <Route path='/add_product' element={<Addproduct />} />
+            </Routes>
+            <div><Footer /></div>
+          </Router>
+        </div>
+
+      }
+
     </div >
   );
 }
