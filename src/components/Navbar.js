a0f090d3294e682672bcfdb4008d737a7d87742f
mahmoud <mamoidtamer300@gmail.com>
2023-08-03 11:46:29 +0300

    2

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,7 +14,7 @@ function Navbar() {
                             <NavLink className="nav-link" aria-current="page" to='/'>Home</NavLink>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" aria-current="page" href="https://github.com/mahmoudtamer0/Masr.market/" target="_blank">Main_Market</a>
+                            <a className="nav-link" aria-current="page" href="https://mahmoudtamer0.github.io/Masr.market/" target="_blank">Main_Market</a>
                         </li>
                     </ul>
                 </div>
